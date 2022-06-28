c97f49064cf83dda01ff219c8c1ae85774480c20
jawoznia <jnwznk@gmail.com>
2022-06-28 18:19:35 +0200

    Continuing with project after a break.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #[macro_use]
 extern crate rocket;
 
+// Download data from some blockchain related server and display it
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
+#[get("/world")] // <- route attribute
+fn world() -> &'static str {
+    // <- request handler
+    "hello, world!"
+}
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/hello", routes![world])
+        .launch()
+        .await?;
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    Ok(())
 }
